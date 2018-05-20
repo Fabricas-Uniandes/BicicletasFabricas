b03ba30587bc77332b0dc7c0a170162974895eaf
eaperador <e.aperador@uniandes.edu.co>
2018-05-20 09:01:02 -0500

    Agregar precio a los accesorios

--- Proyecto FeatureIDE/Bicishare/features/src/main/java/Bicishare/persistencia/entity/Accesorio.java
+++ Proyecto FeatureIDE/Bicishare/features/src/main/java/Bicishare/persistencia/entity/Accesorio.java
@@ -5,110 +5,121 @@ import java.util.ArrayList;
 import javax.persistence.*;
 
 /**
- * 
+ *
  * @author eaperador
  * @generated
  */
 @Entity
 @Table(name = "Accesorio") // , schema="${schema}")
-@NamedQueries({ @NamedQuery(name = "Accesorio.obtenerTodos", query = "select e from Accesorio e") })
+@NamedQueries({
+  @NamedQuery(name = "Accesorio.obtenerTodos", query = "select e from Accesorio e")})
 public class Accesorio {
 
-	/**
-	 * Identificador de la tabla Accesorio.
-	 */
-	@Id
-	// @Column(name = "Accesorio_id")
-	@GeneratedValue(generator = "AccesorioGen", strategy = GenerationType.SEQUENCE)
-	@SequenceGenerator(name = "AccesorioGen", sequenceName = "Accesorio_SEQ", allocationSize = 1)
-	private Long id;
+  /**
+   * Identificador de la tabla Accesorio.
+   */
+  @Id
+  // @Column(name = "Accesorio_id")
+  @GeneratedValue(generator = "AccesorioGen", strategy = GenerationType.SEQUENCE)
+  @SequenceGenerator(name = "AccesorioGen", sequenceName = "Accesorio_SEQ", allocationSize = 1)
+  private Long id;
 
-	public Long getId() {
-		return this.id;
-	}
+  public Long getId() {
+    return this.id;
+  }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	/**
-	 * @generated 1-1-false
-	 */
+  /**
+   * @generated 1-1-false
+   */
+  // @Column(name = "nombre")
+  private String nombre;
 
-	// @Column(name = "nombre")
-	private String nombre;
+  /**
+   * @generated 1-1-false
+   */
+  // @Column(name = "tipo")
+  private String tipo;
 
-	/**
-	 * @generated 1-1-false
-	 */
+  /**
+   * @generated 1-1-false
+   */
+  // @Column(name = "foto")
+  private String foto;
 
-	// @Column(name = "tipo")
-	private String tipo;
+  /*if[Pago]*/
+  private int precio;
+  /*end[Pago]*/
+  
+  /**
+   * @generated 0--1-true
+   */
+  @OneToMany(cascade = {}, fetch = javax.persistence.FetchType.LAZY)
+  private List<Prestamo> prestamo;
 
-	/**
-	 * @generated 1-1-false
-	 */
+  /**
+   * @generated
+   */
+  public String getNombre() {
+    return this.nombre;
+  }
 
-	// @Column(name = "foto")
-	private String foto;
+  /**
+   * @generated
+   */
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
 
-	/**
-	 * @generated 0--1-true
-	 */
-	@OneToMany(cascade = {}, fetch = javax.persistence.FetchType.LAZY)
-	private List<Prestamo> prestamo;
+  /**
+   * @generated
+   */
+  public String getTipo() {
+    return this.tipo;
+  }
 
-	/**
-	 * @generated
-	 */
-	public String getNombre() {
-		return this.nombre;
-	}
+  /**
+   * @generated
+   */
+  public void setTipo(String tipo) {
+    this.tipo = tipo;
+  }
 
-	/**
-	 * @generated
-	 */
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+  /**
+   * @generated
+   */
+  public String getFoto() {
+    return this.foto;
+  }
 
-	/**
-	 * @generated
-	 */
-	public String getTipo() {
-		return this.tipo;
-	}
+  /**
+   * @generated
+   */
+  public void setFoto(String foto) {
+    this.foto = foto;
+  }
 
-	/**
-	 * @generated
-	 */
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
+  public List<Prestamo> getPrestamo() {
+    if (prestamo != null) {
+      prestamo = new ArrayList<Prestamo>();
+    }
+    return this.prestamo;
+  }
 
-	/**
-	 * @generated
-	 */
-	public String getFoto() {
-		return this.foto;
-	}
+  public void setPrestamo(List<Prestamo> prestamo) {
+    this.prestamo = prestamo;
+  }
 
-	/**
-	 * @generated
-	 */
-	public void setFoto(String foto) {
-		this.foto = foto;
-	}
+  /*if[Pago]*/
+  public int getPrecio() {
+    return precio;
+  }
 
-	public List<Prestamo> getPrestamo() {
-		if (prestamo != null) {
-			prestamo = new ArrayList<Prestamo>();
-		}
-		return this.prestamo;
-	}
-
-	public void setPrestamo(List<Prestamo> prestamo) {
-		this.prestamo = prestamo;
-	}
-
+  public void setPrecio(int precio) {
+    this.precio = precio;
+  }
+  /*end[Pago]*/
 }
--- Proyecto FeatureIDE/Bicishare/features/src/main/webapp/js/RouteProvider.js
+++ Proyecto FeatureIDE/Bicishare/features/src/main/webapp/js/RouteProvider.js
@@ -20,11 +20,15 @@ app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/MedioDePago', {templateUrl: 'partials/MedioDePago.html', controller: 'MedioDePagoCtrl'});
 	$routeProvider.when('/Convenio', {templateUrl: 'partials/Convenio.html', controller: 'ConvenioCtrl'});
 	$routeProvider.when('/Proveedor', {templateUrl: 'partials/Proveedor.html', controller: 'ProveedorCtrl'});
+        /*if[Bonificaciones]*/
 	$routeProvider.when('/BonificacionGanada', {templateUrl: 'partials/BonificacionGanada.html', controller: 'BonificacionGanadaCtrl'});
+        /*end[Bonificaciones]*/
 	$routeProvider.when('/TarjetaCredito', {templateUrl: 'partials/TarjetaCredito.html', controller: 'TarjetaCreditoCtrl'});
 	$routeProvider.when('/PSE', {templateUrl: 'partials/PSE.html', controller: 'PSECtrl'});
 	$routeProvider.when('/Pago', {templateUrl: 'partials/Pago.html', controller: 'PagoCtrl'});
+        /*if[Gamification]*/
 	$routeProvider.when('/Logro', {templateUrl: 'partials/Logro.html', controller: 'LogroCtrl'});
+        /*end[Gamification]*/
 	$routeProvider.when('/Efectivo', {templateUrl: 'partials/Efectivo.html', controller: 'EfectivoCtrl'});
 	$routeProvider.when('/Usuario', {templateUrl: 'partials/Usuario.html', controller: 'UsuarioCtrl'});
 	$routeProvider.when('/Marca', {templateUrl: 'partials/Marca.html', controller: 'MarcaCtrl'});
