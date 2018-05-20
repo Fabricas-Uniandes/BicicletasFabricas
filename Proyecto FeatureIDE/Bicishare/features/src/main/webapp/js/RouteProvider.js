7a71adc11b7f1421f8426bf92bed9f8bb27cf5c6
eaperador <e.aperador@uniandes.edu.co>
2018-05-20 09:05:04 -0500

    Remover archivos generados, cambios en munge

--- Proyecto FeatureIDE/Bicishare/features/src/main/webapp/js/RouteProvider.js
+++ Proyecto FeatureIDE/Bicishare/features/src/main/webapp/js/RouteProvider.js
@@ -2,35 +2,36 @@
 
 
 // Declare app level module which depends on filters, and services
-var app=angular.module('admininvalid', [
+var app = angular.module('admininvalid', [
   'ngRoute',
   'angularUtils.directives.dirPagination',
   'admininvalid.controllers'
 ]);
-app.config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
-	$routeProvider.when('/Bonificacion', {templateUrl: 'partials/Bonificacion.html', controller: 'BonificacionCtrl'});
-	$routeProvider.when('/Biciusuario', {templateUrl: 'partials/Biciusuario.html', controller: 'BiciusuarioCtrl'});
-	$routeProvider.when('/MedioDePagoUso', {templateUrl: 'partials/MedioDePagoUso.html', controller: 'MedioDePagoUsoCtrl'});
-	$routeProvider.when('/Bicicleta', {templateUrl: 'partials/Bicicleta.html', controller: 'BicicletaCtrl'});
-	$routeProvider.when('/Multa', {templateUrl: 'partials/Multa.html', controller: 'MultaCtrl'});
-        /*if[Accesorios]*/
-	$routeProvider.when('/Accesorio', {templateUrl: 'partials/Accesorio.html', controller: 'AccesorioCtrl'});
-        /*end[Accesorios]*/
-	$routeProvider.when('/Proveedor', {templateUrl: 'partials/Proveedor.html', controller: 'ProveedorCtrl'});
-        /*if[Bonificaciones]*/
-	$routeProvider.when('/BonificacionGanada', {templateUrl: 'partials/BonificacionGanada.html', controller: 'BonificacionGanadaCtrl'});
-        /*end[Bonificaciones]*/
-		/*if[Pago]*/
-	$routeProvider.when('/Pago', {templateUrl: 'partials/Pago.html', controller: 'PagoCtrl'});
-		/*end[Pago]*/
-	$routeProvider.when('/Logro', {templateUrl: 'partials/Logro.html', controller: 'LogroCtrl'});
-        /*end[Gamification]*/
-	$routeProvider.when('/Usuario', {templateUrl: 'partials/Usuario.html', controller: 'UsuarioCtrl'});
-	$routeProvider.when('/Marca', {templateUrl: 'partials/Marca.html', controller: 'MarcaCtrl'});
-	$routeProvider.when('/Prestamo', {templateUrl: 'partials/Prestamo.html', controller: 'PrestamoCtrl'});
-	
-  	$routeProvider.otherwise({redirectTo: '/'}); 
-}]);
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
+    $routeProvider.when('/Bonificacion', {templateUrl: 'partials/Bonificacion.html', controller: 'BonificacionCtrl'});
+    $routeProvider.when('/Biciusuario', {templateUrl: 'partials/Biciusuario.html', controller: 'BiciusuarioCtrl'});
+    $routeProvider.when('/MedioDePagoUso', {templateUrl: 'partials/MedioDePagoUso.html', controller: 'MedioDePagoUsoCtrl'});
+    $routeProvider.when('/Bicicleta', {templateUrl: 'partials/Bicicleta.html', controller: 'BicicletaCtrl'});
+    $routeProvider.when('/Multa', {templateUrl: 'partials/Multa.html', controller: 'MultaCtrl'});
+    /*if[Accesorios]*/
+    $routeProvider.when('/Accesorio', {templateUrl: 'partials/Accesorio.html', controller: 'AccesorioCtrl'});
+    /*end[Accesorios]*/
+    $routeProvider.when('/Proveedor', {templateUrl: 'partials/Proveedor.html', controller: 'ProveedorCtrl'});
+    /*if[Bonificaciones]*/
+    $routeProvider.when('/BonificacionGanada', {templateUrl: 'partials/BonificacionGanada.html', controller: 'BonificacionGanadaCtrl'});
+    /*end[Bonificaciones]*/
+    /*if[Pago]*/
+    $routeProvider.when('/Pago', {templateUrl: 'partials/Pago.html', controller: 'PagoCtrl'});
+    /*end[Pago]*/
+    /*if[Gamification]*/
+    $routeProvider.when('/Logro', {templateUrl: 'partials/Logro.html', controller: 'LogroCtrl'});
+    /*end[Gamification]*/
+    $routeProvider.when('/Usuario', {templateUrl: 'partials/Usuario.html', controller: 'UsuarioCtrl'});
+    $routeProvider.when('/Marca', {templateUrl: 'partials/Marca.html', controller: 'MarcaCtrl'});
+    $routeProvider.when('/Prestamo', {templateUrl: 'partials/Prestamo.html', controller: 'PrestamoCtrl'});
 
-var module=angular.module("admininvalid.controllers",[]);
+    $routeProvider.otherwise({redirectTo: '/'});
+  }]);
+
+var module = angular.module("admininvalid.controllers", []);
